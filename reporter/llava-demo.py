bbd45676c0d89a3f96c4bb7a5a8a8792ceb01130
Oliver Chang <oliverc1622@gmail.com>
2024-02-01 12:27:49 -0800

    while prompt

--- reporter/llava-demo.py
+++ reporter/llava-demo.py
@@ -17,7 +17,9 @@ model_id = "llava-hf/llava-1.5-7b-hf"
 
 pipe = pipeline(model=model_id, model_kwargs={"quantization_config": quantization_config})
 
-max_new_tokens = 200
-prompt = "USER: <image>\nDescribe this driving scene. I am the yellow car. My throttle speed is 0.00930562149733305 and steering angle is 0.2912810146808624. My coordinate is (100.0, 3.999999910593033).\nASSISTANT:"
-
-outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+while 1:
+    prompt = input("USER: <image>\n")
+    prompt += "\nAssistant:"
+    max_new_tokens = 200
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    print(outputs[0])
