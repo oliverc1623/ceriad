a1f0501f1c747a4f3a570ea05285904c78584653
Oliver Chang <oliverc1622@gmail.com>
2024-02-01 12:52:41 -0800

    image args

--- reporter/llava-demo.py
+++ reporter/llava-demo.py
@@ -1,25 +1,38 @@
+import argparse
 import requests
 from PIL import Image
-
-image = Image.open("../frames/frame_000.png")
-
 import torch
 from transformers import BitsAndBytesConfig
-
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16
-)
-
 from transformers import pipeline
 
-model_id = "llava-hf/llava-1.5-7b-hf"
 
-pipe = pipeline(model=model_id, model_kwargs={"quantization_config": quantization_config})
+def main(args):
+    image = Image.open(args.filename)
+    print(image)
 
-while 1:
-    prompt = input("USER: \n")
-    prompt = "USER: <image>\n" + prompt + "\nAssistant:"
-    max_new_tokens = 200
-    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
-    print(outputs[0]["generated_text"])
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+    )
+
+    model_id = "llava-hf/llava-1.5-7b-hf"
+
+    pipe = pipeline(
+        model=model_id, model_kwargs={"quantization_config": quantization_config}
+    )
+
+    while 1:
+        prompt = input("Prompt: \n")
+        prompt = "USER: <image>\n" + prompt + "\nAssistant:"
+        max_new_tokens = 200
+        outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+        print(outputs[0]["generated_text"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="LLaVA Inference Demo",
+        description="Runs LLaVA",
+    )
+    parser.add_argument("-f", "--filename", type=str)
+    args = parser.parse_args()
+    main(args)
