4d5fa997583a7f270ca32329f82a37b97dad97ca
William Self <williamself@Williams-MacBook-Pro.local>
2024-01-31 16:22:48 -0800

    Added basic environment render code

---
+++ renderbasicenv.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+from matplotlib import pyplot as plt
+
+env = gym.make('highway-v0', render_mode='rgb_array')
+env.reset()
+for _ in range(3):
+    action = env.action_type.actions_indexes["IDLE"]
+    obs, reward, done, truncated, info = env.step(action)
+    env.render()
+
+# Get the image from the environment and display it using matplotlib
+image = env.render()
+plt.imshow(image)
+plt.show()
