4069141bec85aac0f10e7494df2f58371fa09d58
William Self <williamself@Williams-MacBook-Pro.local>
2024-01-31 16:40:06 -0800

    updated test render

--- renderbasicenv.py
+++ renderbasicenv.py
@@ -3,6 +3,8 @@ from matplotlib import pyplot as plt
 
 env = gym.make('highway-v0', render_mode='rgb_array')
 env.reset()
+
+#render the environment at each step, idling for all steps
 for _ in range(3):
     action = env.action_type.actions_indexes["IDLE"]
     obs, reward, done, truncated, info = env.step(action)
