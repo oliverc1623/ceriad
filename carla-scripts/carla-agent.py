f1b00946a765fac4d4c13d452005d09dc5102035
oliverc1623 <oliverc1622@gmail.com>
2024-02-20 16:39:07 +0000

    semantic labels and frames now sync

--- carla-scripts/carla-agent.py
+++ carla-scripts/carla-agent.py
@@ -5,11 +5,13 @@ import math
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
 from carla_gym.multi_env import MultiActorCarlaEnv, DISCRETE_ACTIONS
 from carla_gym.carla_api.PythonAPI.agents.navigation.basic_agent import BasicAgent
 from carla_gym.core.maps.nav_utils import get_next_waypoint
 
+step = 0
 
 def vehicle_control_to_action(vehicle_control, is_discrete):
     """Vehicle control object to action."""
@@ -44,91 +46,69 @@ def process_image(image):
     # The object class is encoded in the red channel.
     semantic_image = image_data[:, :, 2]  # Using the red channel for class information
 
-    num_none = np.sum(semantic_image == 0)  # Assuming '8' is the class ID for vehicles
-    print(f"Number of none pixels: {num_none}")
+    num_none = np.sum(semantic_image == 0)
+    num_roads = np.sum(semantic_image == 1)
+    num_sidewalk = np.sum(semantic_image == 2)
+    num_building = np.sum(semantic_image == 3)
+    num_wall = np.sum(semantic_image == 4)
+    num_fence = np.sum(semantic_image == 5)
+    num_pole = np.sum(semantic_image == 6)
+    num_trafficlight = np.sum(semantic_image == 7)
+    num_trafficsign = np.sum(semantic_image == 8)
+    num_vegetation = np.sum(semantic_image == 9)
+    num_terrian = np.sum(semantic_image == 10)
+    num_sky = np.sum(semantic_image == 11)
+    num_pedestrian = np.sum(semantic_image == 12)
+    num_rider = np.sum(semantic_image == 13)
+    num_car = np.sum(semantic_image == 14)
+    num_truck = np.sum(semantic_image == 15)
+    num_bus = np.sum(semantic_image == 16)
+    num_train = np.sum(semantic_image == 17)
+    num_motorcycle = np.sum(semantic_image == 18)
+    num_bicycle = np.sum(semantic_image == 19)
+    num_static = np.sum(semantic_image == 20)
+    num_dynamic = np.sum(semantic_image == 21)
+    num_other = np.sum(semantic_image == 22)
+    num_water = np.sum(semantic_image == 23)
+    num_RoadLine = np.sum(semantic_image == 24)
+    num_Ground = np.sum(semantic_image == 25)
+    num_bridge = np.sum(semantic_image == 26)
 
-    num_roads = np.sum(semantic_image == 1)  # Assuming '8' is the class ID for vehicles
-    print(f"Number of road pixels: {num_roads}")
+    objects = {
+        "None": num_none,
+        "Road": num_roads,
+        "Sidewalk": num_sidewalk,
+        "Building": num_building,
+        "Wall": num_wall,
+        "Fence": num_fence,
+        "Pole": num_pole,
+        "Traffic light": num_trafficlight,
+        "Traffic sign": num_trafficsign,
+        "Vegetation": num_vegetation,
+        "Terrian": num_terrian,
+        "Sky": num_sky,
+        "Pedestrian": num_pedestrian,
+        "Rider": num_rider,
+        "Car": num_car,
+        "Truck": num_truck,
+        "Bus": num_bus,
+        "Train": num_train,
+        "Motorcycle": num_motorcycle,
+        "Bicycle": num_bicycle,
+        "Static": num_static,
+        "Dynamic": num_dynamic,
+        "Other": num_other,
+        "Water": num_water,
+        "Roadline": num_RoadLine,
+        "Ground": num_Ground,
+        "Bridge": num_bridge
+    }
 
-    num_sidewalk = np.sum(semantic_image == 2)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of sidewalk pixels: {num_sidewalk}")
-
-    num_building = np.sum(semantic_image == 3)  # Assuming '8' is the class ID for vehicles
-    print(f"Number of building pixels: {num_building}")
-
-    num_wall = np.sum(semantic_image == 4)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of wall pixels: {num_wall}")
-
-    num_fence = np.sum(semantic_image == 5)  # Assuming '8' is the class ID for vehicles
-    print(f"Number of fence pixels: {num_fence}")
-
-    num_pole = np.sum(semantic_image == 6)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of pole pixels: {num_pole}")
-
-    num_trafficlight = np.sum(semantic_image == 7)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of traffic light pixels: {num_trafficlight}")
-
-    num_trafficsign = np.sum(semantic_image == 8)  # Assuming '8' is the class ID for vehicles
-    print(f"Number of traffic sign pixels: {num_trafficsign}")
-
-    num_vegetation = np.sum(semantic_image == 9)  # Assuming '8' is the class ID for vehicles
-    print(f"Number of vegetation pixels: {num_vegetation}")
-
-    num_terrian = np.sum(semantic_image == 10)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of terrian pixels: {num_terrian}")
-
-    num_sky = np.sum(semantic_image == 11)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of sky pixels: {num_sky}")
-
-    num_pedestrian = np.sum(semantic_image == 12)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of pedestrian pixels: {num_pedestrian}")
-
-    num_rider = np.sum(semantic_image == 13)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of rider pixels: {num_rider}")
-
-    num_car = np.sum(semantic_image == 14)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of car pixels: {num_car}")
-
-    num_truck = np.sum(semantic_image == 15)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of truck pixels: {num_truck}")
-
-    num_bus = np.sum(semantic_image == 16)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of bus pixels: {num_bus}")
-
-    num_train = np.sum(semantic_image == 17)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of train pixels: {num_train}")
-
-    num_motorcycle = np.sum(semantic_image == 18)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of motorcycle pixels: {num_motorcycle}")
-
-    num_bicycle = np.sum(semantic_image == 19)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of bicycle pixels: {num_bicycle}")
-
-    num_static = np.sum(semantic_image == 20)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of static pixels: {num_static}")
-
-    num_dynamic = np.sum(semantic_image == 21)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of dynamic pixels: {num_dynamic}")
-
-    num_other = np.sum(semantic_image == 22)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of other pixels: {num_other}")
-
-    num_water = np.sum(semantic_image == 23)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of water pixels: {num_water}")
-
-    num_RoadLine = np.sum(semantic_image == 24)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of road line pixels: {num_RoadLine}")
-
-    num_Ground = np.sum(semantic_image == 25)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of ground pixels: {num_Ground}")
-
-    num_bridge = np.sum(semantic_image == 26)  # Assuming '10' is the class ID for vehicles
-    print(f"Number of bridge pixels: {num_bridge}")
-
-    print("")
+    filtered_values = {k: v for k, v in objects.items() if v > 10000}
+    print(filtered_values)
+    print(f"step: {step}")
 
     return "String from segmented image"
-
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="CARLA Manual Control Client")
@@ -142,12 +122,13 @@ if __name__ == "__main__":
     env = MultiActorCarlaEnv(**args, actor_render_width=224, actor_render_height=224, verbose=False)
     # otherwise for PZ AEC: env = carla_gym.env(**args)
 
-    for _ in range(2):
+    for _ in range(1):
         agent_dict = {}
         obs = env.reset()
         total_reward_dict = {}
         import carla
         ss_bp = env.world.get_blueprint_library().find('sensor.camera.semantic_segmentation')
+        #ss_bp.set_attribute('sensor_tick', '1.0')
 
         # agents init
         for actor_id in env.actor_configs.keys():
@@ -162,18 +143,17 @@ if __name__ == "__main__":
         ss_bp.listen(process_image)
 
         start = time.time()
-        step = 0
         done = env.dones
         while not done["__all__"]:
-            step += 1
             action_dict = {}
             for actor_id, agent in agent_dict.items():
                 action_dict[actor_id] = vehicle_control_to_action(agent.run_step(), env.discrete_action_space)
             obs, reward, term, trunc, info = env.step(action_dict)
             ob = (obs[actor_id] * 255).astype(np.uint8)
-            plt.imsave(f"frames/observation_{step:004}.png", ob)
+            plt.imsave(f"frames/observation_{step:006}.png", ob)
             for actor_id in total_reward_dict.keys():
                 total_reward_dict[actor_id] += reward[actor_id]
             print(":{}\n\t".join(["Step#", "rew", "ep_rew", "done{}"]).format(step, reward, total_reward_dict, done))
+            step += 1
         print(f"{step / (time.time() - start)} fps")
     env.close()
