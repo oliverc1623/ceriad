c85f974b71ab19be7d10352f6443518474692043
Oliver Chang <oliverc1622@gmail.com>
2024-03-12 06:50:20 +0000

    logging

---
+++ carla-scripts/reinforcement-learning/ppo/train.py
@@ -0,0 +1,43 @@
+import gymnasium as gym
+import torch
+import time
+from stable_baselines3 import PPO
+from CarlaEnv import CustomEnv
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecMonitor, VecFrameStack
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
+import os
+
+def make_env(rank: int, seed: int = 0):
+    """
+    Utility function for multiprocessed env.
+
+    :param num_env: the number of environments you wish to have in subprocesses
+    :param seed: the inital seed for RNG
+    :param rank: index of the subprocess
+    """
+    def _init():
+        env = CustomEnv(ego_vehicle='car1')
+        env.reset()
+        return env
+    time.sleep(1)
+    return _init
+
+if __name__=="__main__":
+    # Parallel environments
+    num_cpu = 1
+    vec_env = CustomEnv(ego_vehicle='car1')
+    # SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+
+    # Create log dir
+    log_dir = f"tmp/"
+    os.makedirs(log_dir, exist_ok=True)
+    vec_env = Monitor(vec_env, log_dir)
+
+    model = PPO("CnnPolicy",
+                vec_env,
+                verbose=1,
+                policy_kwargs=dict(normalize_images=False),
+                device="cuda:1")
+    model.learn(total_timesteps=100_000, progress_bar=True)
+    model.save("carla-lanefollow-empty")
