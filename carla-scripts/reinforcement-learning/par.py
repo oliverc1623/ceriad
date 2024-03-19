5cc4955e9ae47af02619c88af979d2f0a4f28c87
Oliver <oliverc1622@gmail.com>
2024-03-19 20:50:34 +0000

    better llava prompting

--- carla-scripts/reinforcement-learning/par.py
+++ carla-scripts/reinforcement-learning/par.py
@@ -9,9 +9,12 @@ import torch
 import time
 from stable_baselines3 import PPO
 from CarlaEnv import CustomEnv
+from HardEnv import HardEnv
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# ## Actor-Reporter-Planner
 
 # ## Actor
 
@@ -20,12 +23,12 @@ import numpy as np
 
 # load model and env
 model = PPO.load("ppo/carla-lanefollow-empty_trial1")
-vec_env = CustomEnv(ego_vehicle='car1')
+vec_env = HardEnv(ego_vehicle='car1')
 
 
 # ## Planner
 
-# In[1]:
+# In[22]:
 
 
 from openai import OpenAI
@@ -33,7 +36,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[2]:
+# In[23]:
 
 
 # OpenAI ChatCompletions API client initialization
@@ -42,29 +45,29 @@ client = OpenAI(
 )
 
 
-# In[3]:
+# In[24]:
 
 
 def selectAction(prompt):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
         messages=[
-            {"role": "system", "content": "Your goal is to drive safely and efficiently. You are directing the ego vehicle in our simulation, selecting actions when prompted. Respond with the action name only, and nothing else."},
+            {"role": "system", "content": "Your goal is to drive safely and efficiently. You are directing the ego vehicle in our simulation, selecting actions when prompted."},
             {"role": "user", "content": prompt}
         ]
     )
     return completion.choices[0].message.content.strip()
 
 
-# In[ ]:
+# In[28]:
 
 
-selectAction("There is a car right in front of me.")
+selectAction("There is a motocycle directly in front of me that appears to be stoppping. Select 1) brake or 2) continue for me.")
 
 
 # ## Reporter
 
-# In[8]:
+# In[6]:
 
 
 from llava.model.builder import load_pretrained_model
@@ -72,61 +75,24 @@ from llava.mm_utils import get_model_name_from_path
 from llava.eval.run_llava import eval_model
 
 
-# In[4]:
-
-
-model_path = "liuhaotian/llava-v1.5-7b"
-prompt = "What are the things I should be cautious about when I visit here?"
-image_file = "https://llava-vl.github.io/static/images/view.jpg"
-
-args = type('Args', (), {
-    "model_path": model_path,
-    "model_base": None,
-    "model_name": get_model_name_from_path(model_path),
-    "query": prompt,
-    "conv_mode": None,
-    "image_file": image_file,
-    "sep": ",",
-    "temperature": 0,
-    "top_p": None,
-    "num_beams": 1,
-    "max_new_tokens": 512
-})()
-eval_model(args)
-
-
-# ## Actor-Reporter-Planner
-
-# In[3]:
+# In[7]:
 
 
 obs, info = vec_env.reset()
 
 
-# In[4]:
-
-
-vis_img = vec_env.render()
-
-
-# In[9]:
-
-
-obs['image'].shape
-
-
-# In[12]:
+# In[8]:
 
 
 plt.imsave("basic.jpg", obs['image'][0],cmap='gray')
 
 
-# In[9]:
+# In[29]:
 
 
 # get initial llava report
 model_path = "liuhaotian/llava-v1.5-7b"
-prompt = "What are objects worth noting in this driving scene and what are potential hazards?"
+prompt = "What are objects worth noting in the current scenario? What are the vehicles in the image, their positions, and are they a possible threat to the ego vehicle based on their driving? Answer as the point of view of the driver in the image. Be as succinct as possible."
 image_file = "basic.jpg"
 
 args = type('Args', (), {
@@ -145,7 +111,19 @@ args = type('Args', (), {
 eval_model(args)
 
 
-# In[ ]:
+# In[30]:
+
+
+report = "In the image, there is a black car driving down the road, and another car is following it. The black car is in the foreground, and the other car is in the background. The black car is driving in front of the other car, and the driver of the black car might feel a sense of superiority or ego. However, the driver should be cautious and aware of the other car's position, as it could potentially cause an accident if the driver does not maintain a safe distance or if the other car decides to overtake the black car."
+report += " Select 1) brake or 2) continue for me."
+selectAction(report)
+
+
+# Vehicle obstacle test:
+# Trial 1: 
+# 
+
+# In[17]:
 
 
 # while True:
@@ -153,4 +131,10 @@ eval_model(args)
 #     obs, rewards, dones, truncated, info = vec_env.step(action)
 #     if dones:
 #         obs, info = vec_env.reset()
+
+
+# In[ ]:
+
+
+
 
